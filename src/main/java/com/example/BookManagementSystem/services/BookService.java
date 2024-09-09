a14c6a28834c33cf60999cfa02fd0042e2b166f5
Reynard A. <reynardaurele1510@gmail.com>
2024-09-09 17:53:28 +0700

    -

--- src/main/java/com/example/BookManagementSystem/services/BookService.java
+++ src/main/java/com/example/BookManagementSystem/services/BookService.java
@@ -1,10 +1,12 @@
 package com.example.BookManagementSystem.services;
 
 import com.example.BookManagementSystem.entity.BookEntity;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface BookService {
     BookEntity saveBook(BookEntity book);
 
