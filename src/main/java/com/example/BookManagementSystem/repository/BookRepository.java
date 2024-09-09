0328461219ba915594cbe80fa0427ae927f903d4
jaflanet <jonathan.aurelius@ui.ac.id>
2024-09-09 16:26:26 +0700

    repository

--- src/main/java/com/example/BookManagementSystem/repository/BookRepository.java
+++ src/main/java/com/example/BookManagementSystem/repository/BookRepository.java
@@ -1,9 +1,21 @@
 package com.example.BookManagementSystem.repository;
 
 
+import com.example.BookManagementSystem.entity.AuthorEntity;
 import com.example.BookManagementSystem.entity.BookEntity;
+import com.example.BookManagementSystem.entity.GenreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<BookEntity>, Long> {
+import java.awt.print.Book;
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+    List<BookEntity> findByTitleContainingOrAuthor_NameContainingOrGenre_NameContaining(
+            String title, String authorName, String genreName);
+}
+
+public interface AuthorRepository extends JpaRepository<AuthorEntity, Long> {}
+
+public interface GenreRepository extends JpaRepository<GenreEntity, Long> {}
 
 }
