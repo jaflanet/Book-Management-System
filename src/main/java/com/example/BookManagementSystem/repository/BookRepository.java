7bbbce3b5a7e373f0c1324290d9f3917e8272a94
Reynard A. <reynardaurele1510@gmail.com>
2024-09-09 17:06:22 +0700

    -

--- src/main/java/com/example/BookManagementSystem/repository/BookRepository.java
+++ src/main/java/com/example/BookManagementSystem/repository/BookRepository.java
@@ -4,6 +4,8 @@ package com.example.BookManagementSystem.repository;
 import com.example.BookManagementSystem.entity.AuthorEntity;
 import com.example.BookManagementSystem.entity.BookEntity;
 import com.example.BookManagementSystem.entity.GenreEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.awt.print.Book;
@@ -11,8 +13,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
-    List<BookEntity> findByTitleContainingOrAuthor_NameContainingOrGenre_NameContaining(
-            String title, String authorName, String genreName);
+//    List<BookEntity> findByTitleContainingOrAuthor_NameContainingOrGenre_NameContaining(
+//            String title, String authorName, String genreName);
+
+    Page<BookEntity> findAll(Pageable pageable);
 }
 
 //public interface AuthorRepository extends JpaRepository<AuthorEntity, Long> {
