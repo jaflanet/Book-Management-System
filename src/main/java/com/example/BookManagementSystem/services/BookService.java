44317c89f3367ab1c5ff4551240e1edc3e850de8
jaflanet <jonathan.aurelius@ui.ac.id>
2024-09-09 16:35:43 +0700

    services

--- src/main/java/com/example/BookManagementSystem/services/BookService.java
+++ src/main/java/com/example/BookManagementSystem/services/BookService.java
@@ -1,4 +1,18 @@
 package com.example.BookManagementSystem.services;
 
+import com.example.BookManagementSystem.entity.BookEntity;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface BookService {
+    BookEntity saveBook(BookEntity book);
+
+    List<BookEntity> getAllBooks();
+
+    Optional<BookEntity> getBookById(Long id);
+
+    void deleteBook(Long id);
+
+    List<BookEntity> searchBooks(String title, String authorName, String genreName);
 }
--- src/main/java/com/example/BookManagementSystem/services/Impl/BookServiceImpl.java
+++ src/main/java/com/example/BookManagementSystem/services/Impl/BookServiceImpl.java
@@ -1,4 +1,46 @@
 package com.example.BookManagementSystem.services.Impl;
 
-public class BookServiceImpl {
+
+import com.example.BookManagementSystem.entity.BookEntity;
+import com.example.BookManagementSystem.repository.BookRepository;
+import com.example.BookManagementSystem.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public BookEntity saveBook(BookEntity book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<BookEntity> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Optional<BookEntity> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    @Override
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    @Override
+    public List<BookEntity> searchBooks(String title, String authorName, String genreName) {
+        return bookRepository.findByTitleContainingOrAuthor_NameContainingOrGenre_NameContaining(
+                title, authorName, genreName);
+    }
 }
+
+
